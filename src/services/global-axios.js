eca39b00281fa20ba645b087b2fb24ed59f5b33d
ShroukAbdelrazik <beshoymoawed@gmail.com>
2022-02-17 13:54:23 +0200

    trnslate

--- src/services/global-axios.js
+++ src/services/global-axios.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
 let hasToken = null;
+let lang = null;
 
 hasToken = localStorage.getItem("token");
+lang = localStorage.getItem("lang") || "en";
 
 const globalAxios = axios.create({
   baseURL: process.env.VUE_APP_AXSIOS_LINK,
   headers: {
     "Content-Type": "application/json",
     Authorization: hasToken ? `Bearer ${hasToken}` : "",
-    "Accept-Language": "en",
+    "Accept-Language": lang,
   },
 });
 
