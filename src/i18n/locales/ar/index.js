dca56d32697dd7c33e7f0e5e29f3f2ef38d4957e
Mahmoud Hamdy <Mahmoud-El-Deeb@hotmail.com>
2022-03-05 21:01:22 +0200

    feat: :sparkles: Add Contact Us Page
    Add Structure of contact us Page with I18n

--- src/i18n/locales/ar/index.js
+++ src/i18n/locales/ar/index.js
@@ -2,10 +2,12 @@ import register from "./register";
 import home from "./home";
 import login from "./login";
 import profile from "./profile";
+import contactUs from "./contactUs";
 
 export default {
   register,
   home,
   login,
   profile,
+  contactUs
 };
