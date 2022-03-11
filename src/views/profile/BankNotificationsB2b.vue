3237ac99e0f5c0842b799c68700d89804850b206
Mahmoud Hamdy <Mahmoud-El-Deeb@hotmail.com>
2022-03-11 03:01:37 +0200

    feat: :sparkles: Add (Supplier Rating, Quotations, Bank Notification)

---
+++ src/views/profile/BankNotificationsB2b.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h5 class="profileB2b-header-table">
+      {{ $t("profile.bankNotificationsHeader") }}
+    </h5>
+    <b-table
+      hover
+      :items="items"
+      :fields="fields"
+      stacked="lg"
+      show-empty
+      :empty-text="$t('profile.bankNotificationEmpty')"
+    >
+    </b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fields: [
+        {
+          key: "orderNumber",
+          label: this.$t("profile.orderNumber"),
+        },
+        {
+          key: "construction",
+          label: this.$t("profile.construction"),
+        },
+        {
+          key: "batch",
+          label: this.$t("profile.Batch"),
+        },
+        {
+          key: "status",
+          label: this.$t("profile.status"),
+        },
+        {
+          key: "details",
+          label: this.$t("profile.details"),
+        },
+      ],
+      items: [],
+    };
+  },
+};
+</script>
+<style></style>
