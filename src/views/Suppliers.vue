2745b495ff6b0f4212666c81e9ede4c9343d1f05
ahmed reda <ahmed.reda@at-portal.info>
2022-04-11 18:02:01 +0200

    cart

--- src/views/Suppliers.vue
+++ src/views/Suppliers.vue
@@ -34,6 +34,28 @@
             <SingleSupplier :supplier="supplier"></SingleSupplier>
           </div>
         </div>
+        <!-- <div class="overflow-auto">
+          <b-pagination
+            v-model="currentPage"
+            :total-rows="rows"
+            :per-page="perPage"
+            first-text="First"
+            prev-text="Prev"
+            next-text="Next"
+            last-text="Last"
+            pills 
+          ></b-pagination>
+
+          <p class="mt-3">Current Page: {{ currentPage }}</p>
+
+          <b-table
+            id="my-table"
+            :items="suppliers"
+            :per-page="perPage"
+            :current-page="currentPage"
+          ></b-table>
+          
+        </div> -->
       </div>
     </div>
   </div>
@@ -63,6 +85,19 @@ export default {
       ],
       suppliers: null,
       loading: false,
+      perPage:3,
+      currentPage: 1,
+      my_items: [
+        { id: 1, first_name: "Fred", last_name: "Flintstone" },
+        { id: 2, first_name: "Wilma", last_name: "Flintstone" },
+        { id: 3, first_name: "Barney", last_name: "Rubble" },
+        { id: 4, first_name: "Betty", last_name: "Rubble" },
+        { id: 5, first_name: "Pebbles", last_name: "Flintstone" },
+        { id: 6, first_name: "Bamm Bamm", last_name: "Rubble" },
+        { id: 7, first_name: "The Great", last_name: "Gazzoo" },
+        { id: 8, first_name: "Rockhead", last_name: "Slate" },
+        { id: 9, first_name: "Pearl", last_name: "Slaghoople" },
+      ],
     };
   },
   methods: {
@@ -84,6 +119,11 @@ export default {
   },
   mounted() {
     this.getSuppliers();
+  },
+  computed: {
+    rows() {
+      return this.my_items.length;
+    },
   },
 };
 </script>
