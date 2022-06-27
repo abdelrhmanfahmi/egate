314c7506cad3a438d8f6cea5e64371c176823b04
ali hefny <ali.badr.hefny@gmail.com>
2022-06-27 18:17:30 +0200

    0.0.138

--- src/components/layouts/footer.vue
+++ src/components/layouts/footer.vue
@@ -182,7 +182,7 @@
                   }}</a
                   >. {{ $t("home.allRightsReserved") }}
                 </p>
-                <p>version 0.0.137</p>
+                <p>version 0.0.138</p>
               </div>
             </div>
           </div>
