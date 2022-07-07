abd0c39654be2c9c4258d35bd6013fad2520b745
ahmed reda <ahmed.reda@at-portal.info>
2022-07-07 16:40:55 +0200

    fix bugs

--- src/components/single-product/ProductInfo.vue
+++ src/components/single-product/ProductInfo.vue
@@ -935,7 +935,7 @@ textarea {
       background: #fff;
       color: #312620;
       box-shadow: 0px 1px 5px 0px rgb(0 0 0 / 10%);
-      padding: 1rem 2rem;
+      padding: 1rem 1rem;
       border-radius: 0.2rem;
       margin: 0 0.3rem;
       &:hover {
