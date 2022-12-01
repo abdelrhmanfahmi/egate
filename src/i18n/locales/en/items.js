d686e47c1bc10471d64e29c294fe5059cb5f31a7
ahmed reda <ahmed.reda@at-portal.info>
2022-12-01 11:59:41 +0200

    edit client bugs

--- src/i18n/locales/en/items.js
+++ src/i18n/locales/en/items.js
@@ -38,6 +38,6 @@ export default {
   remove:'Remove',
   addAllToCart:'Add All To Cart',
   removeGroup:'Remove This Group',
-  backToStrandingList:'Back To Groups',
+  backToStrandingList:'Back To Standing Orders',
   advertise:'Ad'
 };
