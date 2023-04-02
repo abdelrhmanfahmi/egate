188ae15d04507e4e8c0b2217bde1c9b85fd773b6
ahmed reda <ahmed.reda@at-portal.info>
2023-04-02 14:49:11 +0200

    change design of popup cart in navbar

--- src/components/cart/CartItem.vue
+++ src/components/cart/CartItem.vue
@@ -14,62 +14,86 @@
     <!-- else  -->
     <div class="" v-else>
       <div
-        class="d-flex cart-item"
+        class="cart-item"
         v-for="product in products.products"
         :key="product.id"
       >
-        <a
-          @click="goProduct(product)"
-          class="product-img-container w-25"
-        >
-          <img
-            :src="product.product_image"
-            alt="Cart Item"
-            class="product-image"
-            v-if="product.product_image"
-          />
-          <img
-          v-else-if="product.basket_image"
-            :src="product.basket_image"
-            alt="Cart Item"
-            class="product-image"
-          />
-        </a>
-        <div class="product-info w-50">
-          <a @click="goProduct(product)" class="name">
-            <span v-if="product.product_name">{{ product.product_name }}</span>
-            <span v-else-if="product.basket_name">{{ product.basket_name }}</span>
-          </a>
+        <div class="row">
+          <div class="col-md-8 col-sm-12 mb-3">
+            <div class="d-flex justify-content-start align-items-center">
+              <a @click="goProduct(product)" class="product-img-container ">
+                <img
+                  :src="product.product_image"
+                  alt="Cart Item"
+                  class="product-image"
+                  v-if="product.product_image"
+                />
+                <img
+                  v-else-if="product.basket_image"
+                  :src="product.basket_image"
+                  alt="Cart Item"
+                  class="product-image"
+                />
+              </a>
+              <div class="product-info">
+                <a @click="goProduct(product)" class="name">
+                  <span v-if="product.product_name">{{
+                    product.product_name
+                  }}</span>
+                  <span v-else-if="product.basket_name">{{
+                    product.basket_name
+                  }}</span>
+                </a>
+                <Counter
+                  :minimum="
+                    product.min_order_quantity ? product.min_order_quantity : 1
+                  "
+                  :quantity="product.quantity"
+                  :product="product"
+                  class="justify-content-center"
+                  v-if="!product.gift_promotion_id"
+                ></Counter>
 
-          <span class="price" v-if="product.price || product.price >=0" >
-            <span v-if="!product.gift_promotion_id">{{ product.price | fixedCurrency }}</span>
-          </span>
-          <span class="price" v-if="!product.gift_promotion_id"> x </span>
-          <span class="price" v-if="!product.gift_promotion_id">
-            <b class="text-danger font-weight-bold">{{ product.quantity }} </b>
-          </span>
-          <span class="text-danger font-weight-bold" v-if="product.gift_promotion_id">{{product.quantity}}</span>
-        </div>
-        <div class="total mx-1 text-center w-50">
+                <span
+                  class="text-danger font-weight-bold"
+                  v-if="product.gift_promotion_id"
+                  >{{ product.quantity }}</span
+                >
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-12">
 
-          <Counter
-            :minimum="
-              product.min_order_quantity ? product.min_order_quantity : 1
-            "
-            :quantity="product.quantity"
-            :product="product"
-            class="justify-content-center"
-            v-if="!product.gift_promotion_id"
-          ></Counter>
-          <p class="product_sub_total mt-2" v-if="!product.gift_promotion_id">
-            {{ product.product_sub_total | fixedCurrency }} {{ currency }}
-          </p>
-          <img src="@/assets/images/giftbox.png" v-if="product.gift_promotion_id" class="gift-product" alt="gift-product">
-        </div>
-        <div class="actions mx-1" @click="removeFromCart(product)" v-if="!product.gift_promotion_id">
-          <span class="action-icon">
-            <b-icon-trash></b-icon-trash>
-          </span>
+            <div class="total mx-1 text-center">
+              <!-- <span class="price" v-if="product.price || product.price >=0" >
+                <span v-if="!product.gift_promotion_id">{{ product.price | fixedCurrency }}</span>
+              </span>
+              <span class="price" v-if="!product.gift_promotion_id"> x </span>
+              <span class="price" v-if="!product.gift_promotion_id">
+                <b class="text-danger font-weight-bold">{{ product.quantity }} </b>
+              </span> -->
+    
+              <p class="product_sub_total mt-0" v-if="!product.gift_promotion_id">
+                {{ product.product_sub_total | fixedCurrency }} {{ currency }}
+              </p>
+              <img
+                src="@/assets/images/giftbox.png"
+                v-if="product.gift_promotion_id"
+                class="gift-product"
+                alt="gift-product"
+              />
+              <div
+                class="actions mx-1"
+                @click="removeFromCart(product)"
+                v-if="!product.gift_promotion_id"
+              >
+                <span class="action-icon">
+                  <!-- <b-icon-trash></b-icon-trash> -->
+                  {{ $t("items.remove") }}
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -77,7 +101,7 @@
 </template>
 <script>
 // This is a description of the cart items data
-import { BIconTrash } from "bootstrap-vue";
+// import { BIconTrash } from "bootstrap-vue";
 /**
  * import cart counter
  */
@@ -88,7 +112,7 @@ export default {
     return { count: 0, loading: false };
   },
   components: {
-    BIconTrash,
+    // BIconTrash,
     Counter,
   },
   /**
@@ -124,10 +148,16 @@ export default {
     goProduct(product) {
       this.$router.push(
         {
-          path: product.basket_promotion_id ?  "/basketOfferDetails" : "/details" ,
-          query: { id: product.basket_promotion_id ? product.basket_promotion_id : product.product_supplier_id },
+          path: product.basket_promotion_id
+            ? "/basketOfferDetails"
+            : "/details",
+          query: {
+            id: product.basket_promotion_id
+              ? product.basket_promotion_id
+              : product.product_supplier_id,
+          },
         },
-        // reload after go to product page 
+        // reload after go to product page
         () => {
           this.$router.go(0);
         }
@@ -138,8 +168,9 @@ export default {
 </script>
 <style lang="scss" scoped>
 .action-holder {
-  max-height: 400px;
-  overflow-y: scroll;
+  max-height: 430px;
+  overflow-x: hidden;
+  width:100%
 }
 
 .action-holder {
@@ -190,7 +221,7 @@ export default {
   margin-inline-start: 1px;
   .product-image {
     width: 100%;
-    height: 100%;
+    height: 70px;
     object-fit: cover;
     border-radius: 8px;
   }
@@ -202,5 +233,4 @@ export default {
 .cart-item .product-info .name {
   margin-bottom: 0.5rem;
 }
-
 </style>
