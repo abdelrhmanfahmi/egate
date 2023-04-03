4422f22a1068e8ae2a3c68a0dc2e984361438fea
ahmed reda <ahmed.reda@at-portal.info>
2023-04-03 17:59:40 +0200

    fix quantity

--- src/components/global/Counter.vue
+++ src/components/global/Counter.vue
@@ -13,7 +13,7 @@
             id=""
             min="1"
             @keyup="CustomIncrementQuantity"
-            v-model="currentValue"
+            v-model="countValue"
           />
         </slot>
       </main>
@@ -167,12 +167,20 @@ export default {
       }, 1500);
     },
   },
-  computed:{
-    // check if quantity greater than or not in case of buy x get y offers 
-    currentValue(){
-      return this.countValue > this.quantity ? this.countValue : this.quantity 
-    }
-  }
+  computed: {
+    // check if quantity greater than or not in case of buy x get y offers
+    // currentValue() {
+    //   return this.countValue > this.quantity ? this.countValue : this.quantity;
+    // },
+    // currentValue: {
+    //   get() {
+    //     return this.currentValue;
+    //   },
+    //   set() {
+    //     return this.countValue > this.quantity ? this.countValue : this.quantity;
+    //   },
+    // },
+  },
 };
 </script>
 
