48cce0560b76e5e0a383c014bd709908d92361ca
ahmed reda <ahmed.reda@at-portal.info>
2023-04-12 16:16:23 +0200

    fix addresses

--- src/views/profile/AdressBook.vue
+++ src/views/profile/AdressBook.vue
@@ -394,8 +394,8 @@ export default {
      */
     getAllAdresses() {
       profile.getAllAdresses().then((res) => {
+        console.log('adresses' , res);
         this.adresses = res.data.items;
-        console.log(res);
       });
     },
     // Countires
