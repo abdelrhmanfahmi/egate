92fb16eb13f7d56078be8bb1706afa4591800ba3
ahmed reda <ahmed.reda@at-portal.info>
2023-04-17 21:28:25 +0200

    add related offers tab in product page

--- src/components/relatedOffers.vue
+++ src/components/relatedOffers.vue
@@ -7,6 +7,9 @@ export default {
   data() {
     return {
       offers: null,
+      basket_promotions: null,
+      buy_get_promotions: null,
+      gift_promotions: null,
     };
   },
   methods: {
@@ -15,6 +18,10 @@ export default {
         .getRelatedOffers(this.$route.query.id)
         .then((res) => {
           console.log("res offers", res);
+          // let Offers = null ;
+          this.basket_promotions = res.data.items.basket_promotions
+          this.buy_get_promotions = res.data.items.buy_get_promotions
+          this.gift_promotions = res.data.items.gift_promotions
         })
         .catch((err) => {
           console.log(err);
