6b3f654290f43152bc101b83a40cd267380ffc1f
ahmed reda <ahmed.reda@at-portal.info>
2023-05-11 15:29:06 +0300

    add new and old replace and refund

---
+++ src/components/pages/returns/OldReturnRefund.vue
@@ -0,0 +1,494 @@
+<template>
+  <!-- return refund page  -->
+  <div>
+    <div class="container">
+      <div class="row justify-content-center align-items-center py-5">
+        <div class="col-md-7 col-sm-12">
+          <div class="refund-options">
+            <h2 class="title">
+              {{ $t("profile.refundMethods") }}
+            </h2>
+            <div class="methods">
+              <div class="d-flex justify-content-center align-items-center">
+                <div class="method">
+                  <!-- select option  -->
+                  <div
+                    class="custom-control custom-radio custom-control-inline"
+                  >
+                    <input
+                      type="radio"
+                      id="wallet"
+                      name="paymentMethod"
+                      class="custom-control-input"
+                      v-model="returnData.refund_option"
+                      value="0"
+                    />
+                    <label class="custom-control-label" for="wallet">
+                      {{ $t("profile.wallet") }}
+                    </label>
+                  </div>
+                </div>
+                <!-- if selected option is bank  -->
+                <div class="method" v-if="selectedOption == 'bank'">
+                  <div
+                    class="custom-control custom-radio custom-control-inline"
+                  >
+                    <input
+                      type="radio"
+                      id="bank"
+                      name="paymentMethod"
+                      class="custom-control-input"
+                      v-model="returnData.refund_option"
+                      value="2"
+                    />
+                    <label class="custom-control-label" for="bank">
+                      {{ $t("payment.bankTransfer") }}
+                    </label>
+                  </div>
+                </div>
+                <!-- if selected option is cach  -->
+                <div class="method" v-if="selectedOption == 'cach'">
+                  <div
+                    class="custom-control custom-radio custom-control-inline"
+                  >
+                    <input
+                      type="radio"
+                      id="cach"
+                      name="paymentMethod"
+                      class="custom-control-input"
+                      v-model="returnData.refund_option"
+                      value="3"
+                    />
+                    <label class="custom-control-label" for="cach">
+                      {{ $t("payment.paymentWhenReceiving") }}
+                    </label>
+                  </div>
+                </div>
+                <!-- if selected option is visa  -->
+                <div
+                  v-if="selectedOption == 'visa'"
+                  class="method d-flex justify-content-between align-content-center"
+                >
+                  <div
+                    class="custom-control custom-radio custom-control-inline"
+                  >
+                    <input
+                      type="radio"
+                      id="visa"
+                      name="paymentMethod"
+                      class="custom-control-input"
+                      v-model="returnData.refund_option"
+                      value="1"
+                    />
+                    <label class="custom-control-label" for="visa">
+                      {{ $t("payment.onlinePayment") }}
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-center align-items-center py-2">
+        <div class="col-md-7 col-sm-12">
+          <form class="returnData mb-5" @submit.prevent="returnOrder">
+            <div class="form-input mb-4">
+              <label>
+                {{ $t("profile.returnReason") }}
+              </label>
+              <!-- select reasons   -->
+              <b-form-select v-model="returnData.return_reason" class="mb-3">
+                <b-form-select-option disabled value="null">{{
+                  $t("cart.selectOption")
+                }}</b-form-select-option>
+                <b-form-select-option
+                  :value="reason.id"
+                  v-for="(reason, index) in reasons"
+                  :key="index"
+                  >{{ reason.name }}</b-form-select-option
+                >
+              </b-form-select>
+            </div>
+
+            <div class="row">
+              <div class="col-4">
+                <label>
+                  {{ $t("profile.ReturnedNumber") }}
+                </label>
+                <div class="product-counter mb-2">
+                  <!-- quantity  -->
+
+                  <div
+                    class="actions d-flex justify-content-center align-items-center"
+                    :class="$i18n.locale"
+                  >
+                    <!-- increment quantity  -->
+
+                    <button
+                      class="product-counter-btn"
+                      @click="decrementQuantity(returnData.quantity)"
+                      :disabled="returnData.quantity == 1"
+                      type="button"
+                    >
+                      <b-icon-dash />
+                    </button>
+
+                    <div class="value">
+                      <span class="product-counter-number">
+                        {{
+                          returnData.quantity ? returnData.quantity : 1
+                        }}</span
+                      >
+                    </div>
+                    <button
+                      class="product-counter-btn"
+                      :class="{ disabledBtn: returnData.quantity >= maxQTY }"
+                      @click="incrementQuantity"
+                      type="button"
+                      :disabled="returnData.quantity >= maxQTY"
+                    >
+                      <b-icon-plus />
+                    </button>
+                    <!-- decrement quantity  -->
+                  </div>
+                </div>
+              </div>
+              <div class="col-8">
+                <label for="CommercialLicense">
+                  {{ $t("profile.returnImage") }}
+                  <span class="text-danger">*</span>
+                </label>
+                <b-form-group>
+                  <!-- upload file  -->
+                  <b-form-file
+                    size="lg"
+                    id="returnImage"
+                    @change="uploadImage"
+                    :placeholder="$t('profile.returnImage')"
+                    drop-placeholder="Drop file here..."
+                  ></b-form-file>
+                </b-form-group>
+                <div
+                  class="error text-start"
+                  v-for="(error, index) in uploadErrors.image"
+                  :key="index"
+                >
+                  {{ error }}
+                </div>
+              </div>
+            </div>
+            <!-- text your message  -->
+            <b-form-textarea
+              v-if="returnData.return_reason == 8"
+              id="textarea-rows"
+              :placeholder="$t('profile.returnReason')"
+              rows="8"
+              v-model="returnData.return"
+            ></b-form-textarea>
+
+            <!-- add new text area if user select bank   -->
+
+            <div class="" v-if="returnData.refund_option == 2">
+              <label for="accountDetails">{{
+                $t("profile.accountDetails")
+              }}</label>
+              <b-form-textarea
+                id="accountDetails"
+                rows="8"
+                v-model="returnData.clinet_bank_info"
+              ></b-form-textarea>
+            </div>
+            <div
+              class="error text-start"
+              v-for="(error, index) in uploadErrors.clinet_bank_info"
+              :key="index"
+            >
+              {{ error }}
+            </div>
+
+            <b-button
+              type="submit"
+              class="saveBtn btn-block py-3 mt-3 bg-main"
+              :disabled="
+                btn1Disabled ||
+                (returnData.quantity >= maxQTY &&
+                  returnData.quantity >= maxQTY &&
+                  returnData.quantity > 1)
+              "
+            >
+              <i class="fa fa-upload"></i> {{ $t("cart.submit") }}
+              <span class="loader" v-if="loading"></span>
+            </b-button>
+          </form>
+          <ul>
+            <li v-for="(item, index) in returnData.image" :key="index">
+              <img :src="item.name" alt="" />
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- returnData.refund_option : {{returnData.refund_option}} -->
+    </div>
+  </div>
+</template>
+
+<script>
+//  return refund page
+import profile from "@/services/profile";
+import { BIconPlus, BIconDash } from "bootstrap-vue";
+export default {
+  data() {
+    return {
+      returnData: {
+        image: null,
+        return_reason: null,
+        item_uuid: this.$route.query.orderId ? this.$route.query.orderId : null,
+        return_option: 0, // refund = 0  , replace = 1
+        refund_option: null, // 0=Wallet,1=Visa,2=Bank,3=Cash
+        return: null,
+        quantity: 1,
+        clinet_bank_info: null,
+      },
+      uploadErrors: [],
+      btn1Disabled: false,
+      loading: false,
+      id: this.$route.query.prodId,
+      selectedOption: null,
+      reasons: null,
+      cancelationReason: null,
+      maxQTY: null,
+    };
+  },
+  methods: {
+    /**
+     * @vuese
+     * this function used to call backend to return user Order
+     */
+    async returnOrder() {
+      this.loading = true;
+      this.btn1Disabled = true;
+
+      let formData = new FormData();
+
+      if (this.returnData.image !== null) {
+        formData.append("image", this.returnData.image);
+      }
+
+      if (this.returnData.return_reason === 8) {
+        formData.append("return_reason", null);
+
+        formData.append("return", this.returnData.return);
+      } else {
+        formData.append("return_reason", this.returnData.return_reason);
+        formData.append("return", "");
+      }
+      formData.append("item_uuid", this.returnData.item_uuid);
+      formData.append("return_option", this.returnData.return_option);
+      formData.append("refund_option", this.returnData.refund_option);
+      formData.append("quantity", this.returnData.quantity);
+      formData.append("clinet_bank_info", this.returnData.clinet_bank_info);
+
+      await profile
+        .returnOrder(formData)
+        .then((res) => {
+          if (res.status == 200) {
+            this.sucessMsg(res.data.message);
+            setTimeout(() => {
+              this.$router.push({
+                path: "ReturnedRequest",
+                query: {
+                  UUID: res.data.items.uuid,
+                },
+              });
+            }, 500);
+          }
+        })
+        .catch((error) => {
+          const err = Object.values(error)[2].data;
+          this.uploadErrors = err.items;
+          this.errMsg(err.message);
+        })
+        .finally(() => {
+          this.loading = false;
+          this.btn1Disabled = false;
+        });
+    },
+    /**
+     * @vuese
+     * this function used to upload Image
+     */
+    uploadImage(event) {
+      // this.returnData.image = event.target.files;
+      this.returnData.image = event.target.files[0];
+      // console.log(this.returnData.image);
+    },
+    /**
+     * @vuese
+     * this function used to show length of files and names
+     */
+    formatNames(files) {
+      return files.length === 1
+        ? files[0].name
+        : `${files.length} files selected`;
+    },
+    /**
+     * @vuese
+     * this function used to get Order Data
+     */
+    getOrderData() {
+      profile
+        .getSingleOrders(this.id)
+        .then((res) => {
+          this.orderData = res.data.items.order;
+
+          if (res.data.items.order.payment_type === "wallet") {
+            this.returnData.refund_option = 0;
+            this.selectedOption = "wallet";
+          }
+          // new edit there will no cach on delivery so it will be bank
+
+          // if (res.data.items.order.payment_type === "visa") {
+          //   this.returnData.refund_option = 1;
+          //   this.selectedOption = "visa";
+          // }
+
+          if (
+            res.data.items.order.payment_type === "bank" ||
+            res.data.items.order.payment_type === "cach" ||
+            res.data.items.order.payment_type === "visa"
+          ) {
+            this.returnData.refund_option = 2;
+            this.selectedOption = "bank";
+          }
+
+          // remove it according to last edit
+
+          // if (res.data.items.order.payment_type === "cach") {
+          //   this.returnData.refund_option = 3;
+          //   this.selectedOption = "cach";
+          // }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    /**
+     * @vuese
+     * this function used to know return Reasons
+     */
+    returnReasons() {
+      profile
+        .returnReasons()
+        .then((res) => {
+          console.log(res);
+          this.reasons = res.data.items;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    /**
+     * @vuese
+     * this function used to increment Quantity
+     */
+    incrementQuantity() {
+      this.returnData.quantity += 1;
+    },
+    /**
+     * @vuese
+     * this function used to check Returned Product Quantity
+     */
+    decrementQuantity() {
+      this.returnData.quantity > 1
+        ? this.returnData.quantity--
+        : this.returnData.quantity == 1;
+    },
+    /**
+     * @vuese
+     * this function used to check Returned Product Quantity
+     */
+    checkReturnedProductQuantity() {
+      profile
+        .checkReturnedProductQuantity(this.$route.query.orderId)
+        .then((res) => {
+          console.log(this.$route.query.orderId);
+          console.log(res);
+          this.maxQTY = res.data.items.quantity;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+  mounted() {
+    if (!this.$route.query.orderId) {
+      this.$router.push("/viewOrderDetails?id=563");
+    }
+    this.getOrderData();
+    this.returnReasons();
+    this.checkReturnedProductQuantity();
+  },
+  components: {
+    BIconPlus,
+    BIconDash,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.refund-options {
+  text-align: center;
+  border: 1px solid #ccc;
+  padding: 20px 30px;
+  border-radius: 7px;
+
+  .title {
+    margin: 20px 0;
+  }
+}
+
+.product-counter {
+  display: flex;
+  align-items: center;
+
+  // justify-content: left;
+  .actions {
+    color: #606266;
+
+    .product-counter-btn {
+      width: 2rem;
+      height: 1.55rem;
+      border-radius: 0;
+      border: 1px solid transparent;
+      color: #606266;
+      background: #eef1f2;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      &:first-child {
+        border-bottom: 1px solid #dcdcdc;
+      }
+    }
+  }
+
+  .value {
+    border-radius: 0;
+    border: 1px solid #f0f0f0;
+    color: #544842;
+    font-weight: 500;
+    width: 6rem;
+    height: 3.1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #fff;
+  }
+}
+.disabledBtn {
+  background: #a6a6a6 !important;
+  color: #fff !important;
+}
+</style>
---
+++ src/components/pages/returns/OldReturnReplace.vue
@@ -0,0 +1,428 @@
+<template>
+  <!-- return replace page  -->
+  <div>
+    <div class="container">
+      <div class="row justify-content-center align-items-center pt-5 mt-5">
+        <h2 class="title">
+          {{ $t("profile.replaceMethods") }}
+        </h2>
+      </div>
+      <div class="row justify-content-center align-items-center pb-5 my-5">
+        <div class="col-7">
+          <form class="returnData mb-5 px-5">
+            <div class="form-input mb-4">
+              <label>
+                {{ $t("profile.returnReason") }}
+              </label>
+              <!-- dropdown for reasons  -->
+              <b-form-select v-model="returnData.return_reason" class="mb-3">
+                <b-form-select-option disabled value="null">{{
+                  $t("cart.selectOption")
+                }}</b-form-select-option>
+                <b-form-select-option
+                  :value="reason.id"
+                  v-for="(reason, index) in reasons"
+                  :key="index"
+                  >{{ reason.name }}</b-form-select-option
+                >
+              </b-form-select>
+            </div>
+            <div class="row">
+              <div class="col-4">
+                <label>
+                  {{ $t("profile.ReturnedNumber") }}
+                </label>
+                <div class="product-counter mb-2">
+                  <div
+                    class="actions d-flex justify-content-center align-items-center"
+                    :class="$i18n.locale"
+                  >
+                    <button
+                      class="product-counter-btn"
+                      @click="decrementQuantity(returnData.quantity)"
+                      :disabled="returnData.quantity == 1"
+                      type="button"
+                    >
+                      <b-icon-dash />
+                    </button>
+                    <div class="value">
+                      <span class="product-counter-number">
+                        {{
+                          returnData.quantity ? returnData.quantity : 1
+                        }}</span
+                      >
+                    </div>
+                    <button
+                      class="product-counter-btn"
+                      :class="{ disabledBtn: returnData.quantity >= maxQTY }"
+                      @click="incrementQuantity"
+                      type="button"
+                      :disabled="returnData.quantity >= maxQTY"
+                    >
+                      <b-icon-plus />
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <!-- upload image of product  -->
+              <div class="col-8">
+                <label for="CommercialLicense">
+                  {{ $t("profile.returnImage") }}
+                  <span class="text-danger">*</span>
+                </label>
+                <b-form-group>
+                  <b-form-file
+                    size="lg"
+                    id="returnImage"
+                    @change="uploadImage"
+                    :placeholder="$t('profile.returnImage')"
+                    drop-placeholder="Drop file here..."
+                  ></b-form-file>
+                </b-form-group>
+                <div
+                  class="error text-start"
+                  v-for="(error, index) in uploadErrors.image"
+                  :key="index"
+                >
+                  {{ error }}
+                </div>
+              </div>
+            </div>
+
+            <!-- enter message  -->
+
+            <b-form-textarea
+              v-if="returnData.return_reason == 8"
+              id="textarea-rows"
+              :placeholder="$t('profile.returnReason')"
+              rows="8"
+              v-model="returnData.return"
+            ></b-form-textarea>
+            <b-button
+              type="submit"
+              
+              class="saveBtn btn-block py-3 mt-3 bg-main"
+              :disabled="
+                btn1Disabled ||
+                (returnData.quantity >= maxQTY && returnData.quantity > 1)
+              "
+              @click.prevent="returnOrder"
+            >
+              <i class="fa fa-upload"></i> {{ $t("cart.submit") }}
+              <span class="loader" v-if="loading"></span>
+            </b-button>
+          </form>
+          <ul>
+            <li v-for="(item, index) in returnData.image" :key="index">
+              <img :src="item.name" alt="" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// return replace page
+// this page used when client need to return his products and using replace method
+import profile from "@/services/profile";
+import { BIconPlus, BIconDash } from "bootstrap-vue";
+export default {
+  data() {
+    return {
+      returnData: {
+        image: null,
+        return_reason: null,
+        item_uuid: this.$route.query.orderId ? this.$route.query.orderId : null,
+        return_option: 1, // refund = 0  , replace = 1
+        refund_option: 0, // 0=Wallet,1=Visa,2=Bank,3=Cash
+        return: null,
+        quantity: 1,
+      },
+      uploadErrors: [],
+      btn1Disabled: false,
+      loading: false,
+      // reasons: null,
+      reasons: null,
+      id: this.$route.query.prodId,
+      orderData: null,
+      maxQTY: null,
+    };
+  },
+  methods: {
+    /**
+     * @vuese
+     * this function used to get Order Data
+     */
+    getOrderData() {
+      profile
+        .getSingleOrders(this.id)
+        .then((res) => {
+          this.orderData = res.data.items.order;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    /**
+     * @vuese
+     * this function used to call backend to return user Order
+     */
+    async returnOrder() {
+      this.loading = true;
+      this.btn1Disabled = true;
+      let formData = new FormData();
+
+      if (this.returnData.image !== null) {
+        formData.append("image", this.returnData.image);
+      }
+
+      if (
+        this.returnData.return_reason === 8 // last id
+      ) {
+        formData.append("return_reason", null);
+
+        formData.append("return", this.returnData.return);
+      } else {
+        formData.append("return_reason", this.returnData.return_reason);
+        formData.append("return", "");
+      }
+      formData.append("item_uuid", this.returnData.item_uuid);
+      formData.append("return_option", this.returnData.return_option);
+      formData.append("refund_option", this.returnData.refund_option);
+      formData.append("quantity", this.returnData.quantity);
+
+      await profile
+        .returnOrder(formData)
+        .then((res) => {
+          if (res.status == 200) {
+            this.sucessMsg(res.data.message);
+
+            setTimeout(() => {
+              this.$router.push({
+                path: "ReturnedRequest",
+                query: {
+                  UUID: res.data.items.uuid,
+                },
+              });
+
+              // this.$router.push("/");
+            }, 500);
+          }
+          console.log(res);
+        })
+        .catch((error) => {
+          const err = Object.values(error)[2].data;
+          this.uploadErrors = err.items;
+          this.errMsg(err.message);
+        })
+        .finally(() => {
+          this.loading = false;
+          this.btn1Disabled = false;
+        });
+    },
+    /**
+     * @vuese
+     * this function used to upload Image
+     */
+    uploadImage(event) {
+      // this.returnData.image = event.target.files;
+      this.returnData.image = event.target.files[0];
+      // console.log(this.returnData.image);
+    },
+    /**
+     * @vuese
+     * this function used to show length of files and names
+     */
+    formatNames(files) {
+      return files.length === 1
+        ? files[0].name
+        : `${files.length} files selected`;
+    },
+    /**
+     * @vuese
+     * this function used to know return Reasons
+     */
+    returnReasons() {
+      profile
+        .returnReasons()
+        .then((res) => {
+          console.log(res);
+          this.reasons = res.data.items;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    /**
+     * @vuese
+     * this function used to increment Quantity
+     */
+    incrementQuantity() {
+      this.returnData.quantity += 1;
+    },
+    /**
+     * @vuese
+     * this function used to decrement Quantity
+     */
+    decrementQuantity() {
+      this.returnData.quantity > 1
+        ? this.returnData.quantity--
+        : this.returnData.quantity == 1;
+    },
+    /**
+     * @vuese
+     * this function used to check Returned Product Quantity
+     */
+    checkReturnedProductQuantity() {
+      profile
+        .checkReturnedProductQuantity(this.$route.query.orderId)
+        .then((res) => {
+          this.maxQTY = res.data.items.quantity;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+  mounted() {
+    // this.getOrderData()
+    this.returnReasons();
+    this.checkReturnedProductQuantity();
+  },
+  components: {
+    BIconPlus,
+    BIconDash,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+/**
+  * component style 
+*/
+.product-counter {
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  .actions {
+    //color: #606266;
+    .product-counter-btn {
+      width: 2rem;
+      height: 1.75rem;
+      border-radius: 0;
+      border: 1px solid transparent;
+      color: #606266;
+      background: #eef1f2;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 50px;
+      &:first-child {
+        border-bottom: 1px solid #dcdcdc;
+      }
+    }
+  }
+  .value {
+    border-radius: 0;
+    border: 1px solid $top-header-color;
+    color: #544842;
+    font-weight: 500;
+    width: 4rem;
+    //height: 3.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #fff;
+    height: 50px;
+  }
+}
+.disabledBtn {
+  background: #a6a6a6 !important;
+  color: #fff !important;
+}
+
+.company-logo {
+  main {
+    //margin-top: -30px;
+    height: 100%;
+  }
+
+  .file-wrapper {
+    text-align: center;
+    height: 5em;
+    vertical-align: middle;
+    display: table-cell;
+    position: relative;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center; /* and other things to make it pretty */
+  }
+
+  .file-wrapper input {
+    position: absolute;
+    top: 0;
+    right: 0; /* not left, because only the right part of the input seems to
+                 be clickable in some browser I can't remember */
+    cursor: pointer;
+    opacity: 0;
+    filter: alpha(
+      opacity=0
+    ); /* and all the other old opacity stuff you
+                                 want to support */
+    font-size: 300px; /* wtf, but apparently the most reliable way to make
+                         a large part of the input clickable in most browsers */
+    height: 200px;
+  }
+  .data-holder {
+    //border: 2px solid $top-header-color;
+    border-radius: 5px;
+    color: #545454;
+    padding: 54px 25px;
+  }
+  .file-input {
+    color: $top-header-color;
+  }
+  .text-holder {
+    color: #bebebe;
+    margin: 20px 0;
+  }
+}
+.file-holder {
+  position: relative;
+  border: 2px solid $gray;
+  margin: 10px 3px 15px;
+  padding: 30px;
+  border-radius: 10px;
+  display: inline-block;
+  min-height: 250px;
+
+  img {
+    width: 150px;
+    height: 200px;
+    border-radius: 10px;
+  }
+  button {
+    border: none;
+    outline: none;
+    position: absolute;
+    right: 0px;
+    bottom: 0px;
+    color: $main-color;
+    font-size: 15px;
+    background: transparent;
+  }
+}
+.method {
+  margin: 10px 0;
+}
+
+button[type="submit"] {
+  width: 50%;
+  @media (max-width: 992px) {
+    width: 100%;
+  }
+}
+</style>
