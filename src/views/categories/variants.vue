5c61328ef29d5e7620770ae935186d82ca8c019b
ahmed reda <ahmed.reda@at-portal.info>
2023-06-13 20:43:06 +0200

    edit pagination rule

--- src/views/categories/variants.vue
+++ src/views/categories/variants.vue
@@ -1110,7 +1110,7 @@
           class="text-center d-flex justify-content-start align-items-center my-2"
         >
           <Paginate
-            v-if="products && this.total && this.totalPages > 1"
+            v-if="products &&  totalPages > 1"
             :total-pages="totalPages"
             :per-page="totalPages"
             :current-page="page"
